ffe964b615ef65bd033f40c352758ffb95dd97a6
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2018-08-24 17:35:15 +0900

    splug best

---
+++ 1874.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[10001];
+int size = 0;
+
+void push(int x){
+    cout<<'+'<<endl;
+    arr[size++] = x;
+}
+int pop(){
+    cout<<'-'<<endl;
+    return  size?arr[--size]:-1;
+}
+int Size(){ return size; }
+bool isEmpty(){ return size?0:1; }
+int top(){ return size?arr[size-1]:-1;}
+int main(){
+    
+}
+
