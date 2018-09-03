993666248e4865645d7c8657a7318ba87a990373
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2018-09-03 20:07:32 +0900

    aa

--- 2504.cpp
+++ 2504.cpp
@@ -1,30 +1,56 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
-
-int arr[31];
-
-int size = 0;
-void push(int x){
-    arr[size++] = x;
-}
-int pop(){
-    return  size?arr[--size]:-1;
-}
-int Size(){ return size; }
-bool isEmpty(){ return size?0:1; }
-int top(){ return size?arr[size-1]:-1;}
 
 int main(){
     char str[31]; cin>>str;
-    int cnt[31]={0};
-    int sz=0;
-    for(int i=0;str[i]!='\n';i++){
-        if(str[i]=='('||str[i]=='['){
-        }
-        else if(str[i]==')'){
-        }
-        else if(str[i]==']'){
+    vector<int> v;
+    int sum = 0;
+    for(int i=0;str[i]!='\0';i++){
+        char c = str[i];
+        if(c == '(') v.push_back(-2);
+        else if(c == '[') v.push_back(-3);
+        else{
+            if(v.empty()){
+                cout<<0<<endl;
+                return 0;
+            }
+            int pop = v.back(); v.pop_back();
+            sum = 0;
+            while(pop>0){
+                sum += pop;
+                if(v.empty()){
+                    cout<<0<<endl;
+                    return 0;
+                }
+                pop = v.back(); v.pop_back();
+            }
+            if(sum == 0) sum = 1;
+            if(c == ')'){
+                if(pop == -3){
+                    cout<<0<<endl;
+                    return 0;
+                }
+                v.push_back(sum*2);
+            }
+            else if(c == ']'){
+                if(pop == -2){
+                    cout<<0<<endl;
+                    return 0;
+                }
+                v.push_back(sum*3);
+            }
         }
     }
+    sum = 0;
+    while(!v.empty()){
+        int back = v.back();
+        if(back<0){
+            cout<<0<<endl;
+            return 0;
+        }
+        sum += back;
+        v.pop_back();
+    }
+    cout<<sum<<endl;
 }
