e6372179799a2366cf6a977dc8f53b38afeaa7e9
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2018-09-03 21:14:51 +0900

    a

--- 1966.cpp
+++ 1966.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<cstring>
 using namespace std;
 
 int arr[100001];
@@ -12,9 +11,44 @@ int Size(){ return back-front-1; }
 bool isEmpty(){ return Size()?0:1; }
 int Front(){return Size()?arr[front+1]:-1;}
 int Back(){return Size()?arr[back-1]:-1;}
+void clear(){ front = 0; back = 1;}
 
+int isMax(){
+    for(int i=front+2;i<back;i++){
+        if(arr[i]>Front()) return 0;
+    }
+    return 1;
+}
 int main(){
-    
-    
+    int T; cin>>T;
+    for(int t=0;t<T;t++){
+        clear();
+        int count = 0;
+        int N,M; cin>>N>>M;
+        int arr2[N];
+        for(int i=0;i<N;i++){
+            cin>>arr2[i];
+            push(arr2[i]);
+        }
+        while(1){
+            if(isMax()){
+                pop();
+                count++;
+                if(M==0){
+                    cout<<count<<endl;
+                    break;
+                }
+                else{
+                    M--;
+                    N--;
+                }
+            }
+            else{
+                push(pop());
+                if(M==0) M = N-1;
+                else M--;
+            }
+        }
+    }
 }
 
