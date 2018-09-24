c22a1c5a37a514634db2415797bb5b46af0d6a47
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2018-09-24 11:20:20 +0900

    grandma

--- 1107.cpp
+++ 1107.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
+
 void makeStack(vector<int>& v,int num){
+    if(num==0)v.push_back(0);
     while(num){
         int mod = num%10;
         v.push_back(mod);
@@ -16,21 +19,116 @@ bool check(int *arr,int n,int num){
     }
     return 1;
 }
+
+
+int queueToInt(queue<int> q){
+    int num=0;
+    while(!q.empty()){
+        num *= 10;
+        num += q.front();
+        q.pop();
+    }
+    return num;
+}
+
 int main(){
     vector<int> v;
+    queue<int> q;
+    
     int N; cin>>N;
     int n; cin>>n;
     int arr[n];
+    
     for(int i=0;i<n;i++) cin>>arr[i];
-    makeStack(v1,N);
+    makeStack(v,N);
     int count = 0;
+    
     while(!v.empty()){
         int num = v.back();
-        if(check(num)){
-            count++;
+        v.pop_back();
+        if(check(arr,n,num)){
+            q.push(num);
         }
         else{
-            
+            int next=10,prev=-1;
+            for(int i=num+1;i<=9;i++){
+                if(check(arr,n,i)){
+                    next = i;
+                    break;
+                }
+            }
+            for(int i=num-1;i>=0;i--){
+                if(check(arr,n,i)){
+                    prev = i;
+                    break;
+                }
+            }
+            cout<<"next,prev : "<<next<<" "<<prev<<endl;
+            if(next-num>num-prev||next==10){
+                int fin = -1;
+                q.push(prev);
+                for(int i=9;i>=0;i--){
+                    if(check(arr,n,i)){
+                        fin = i;
+                        break;
+                    }
+                }
+                for(int i=0;i<v.size();i++){
+                    q.push(fin);
+                }
+            }
+            else if(next-num<num-prev||prev == -1){
+                int fin = 10;
+                q.push(next);
+                for(int i=0;i<=9;i++){
+                    if(check(arr,n,i)){
+                        fin = i;
+                        break;
+                    }
+                }
+                for(int i=0;i<v.size();i++){
+                    q.push(fin);
+                }
+            }
+            else{
+                queue<int> q1,q2;
+                int fin1,fin2;
+                for(int i=9;i>=0;i--){
+                    if(check(arr,n,i)){
+                        fin1 = i;
+                        break;
+                    }
+                }
+                for(int i=0;i<=9;i++){
+                    if(check(arr,n,i)){
+                        fin2 = i;
+                        break;
+                    }
+                }
+                q1=q; q2=q;
+                q1.push(prev); q2.push(next);
+                for(int i=0;i<v.size();i++){q1.push(fin1);q2.push(fin2);}
+                cout<<"q1 : "<<queueToInt(q1)<<endl;
+                cout<<"q2 : "<<queueToInt(q2)<<endl;
+                
+                if(num-queueToInt(q1)>queueToInt(q2)-num) q = q2;
+                else q = q1;
+            }
+            break;
         }
     }
+    
+    for(int i=0;i<q.size();i++)count++;
+    
+    
+    int result = queueToInt(q);
+    cout<<"result : "<<result<<endl;
+    int diff;
+    if(result>N) diff = result-N;
+    else diff = N - result;
+    cout<<"diff : "<<diff<<endl;
+    for(int i=0;i<diff;i++) count++;
+    int newdiff = N>100?N-100:100-N;
+    if(count>newdiff) count = newdiff;
+    cout<<count<<endl;
 }
--- 11723.cpp
+++ 11723.cpp
@@ -1,28 +1,33 @@
 #include<iostream>
 #include<cstring>
-int arr[3000000];
-int size = 0;
 using namespace std;
-
-void add(int);
-void remove(int);
-int check(int);
-void toggle(int);
-void all();
-void empty();
-
 int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
     int M; cin>>M;
+    char command[7];
+    int x;
+    int S = 0;
     for(int i=0;i<M;i++){
-        
+        cin>>command;
+        if(!strcmp("add",command)){
+            cin>>x;
+            S|=(1<<(x-1));
+        }else if(!strcmp("remove",command)){
+            cin>>x;
+            S&=(~(1<<(x-1)));
+        }else if(!strcmp("check",command)){
+            cin>>x;
+            int check = S&(1<<(x-1));
+            if(check) cout<<1<<'\n';
+            else cout<<0<<'\n';
+        }else if(!strcmp("toggle",command)){
+            cin>>x;
+            S^=(1<<(x-1));
+        }else if(!strcmp("all",command)){
+            S = (1<<20)-1;
+        }else if(!strcmp("empty",command)){
+            S = 0;
+        }
     }
-}
-
-void add(int x){
-    if(check(x)) return;
-    arr[size++] = x;
-}
-
-void remove(int x){
-//
 }
